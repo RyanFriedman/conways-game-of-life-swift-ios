f7b38439af7faa819fb727b5a78b2655487e6380
Ryan Friedman <rfriedman@football.com>
2016-05-25 14:00:19 -0700

    created cell and grid classes

--- GameOfLife/Cell.swift
+++ GameOfLife/Cell.swift
@@ -10,12 +10,86 @@ import UIKit
 
 class Cell: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    var row: Int!
+    var col: Int!
+    var alive: Bool = Bool.random()
+    
+    // Need neighboring cells in grid
+    var neighbors: [Cell] = []
+    
+    init(_ frame: CGRect, row: Int, col: Int){
+        super.init(frame: frame)
+        
+        self.row = row
+        self.col = col
+        
+        manageBackgroundColor()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func die() {
+        self.alive = false
+        manageBackgroundColor()
+    }
+    
+    func birth() {
+        self.alive = true
+        manageBackgroundColor()
+    }
+    
+    func manageBackgroundColor() {
+        let color = self.alive ? UIColor.blackColor() : UIColor.whiteColor()
+        self.backgroundColor = color
+    }
 
+    // We return a function to be exectued at a later time,
+    // this ensures a cell doesn't die or birth before it's neighbors
+    // have had a change to evalute themselves
+    func fate() -> () -> () {
+        
+        var livingNeighbors = 0
+        for cell in self.neighbors {
+            if cell.alive { livingNeighbors += 1 }
+        }
+        
+        // Condition 1 Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        // Condition 2 Any live cell with two or three live neighbours lives on to the next generation.
+        // Condition 3 Any live cell with more than three live neighbours dies, as if by over-population.
+
+        if self.alive {
+
+            if livingNeighbors < 2 {
+                return self.die
+            }
+            
+            if livingNeighbors == 2 || livingNeighbors == 3 {
+                return self.birth
+            }
+            
+            if livingNeighbors > 3 {
+                return self.die
+            }
+        }
+        
+        // Condition 4 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        if self.alive == false {
+            
+            if livingNeighbors == 3 {
+                return self.birth
+            }
+        }
+
+        return {}
+        
+    }
+}
+
+extension Bool {
+    static func random() -> Bool {
+        return arc4random_uniform(2) == 0
+    }
 }
--- GameOfLife/Grid.swift
+++ GameOfLife/Grid.swift
@@ -8,11 +8,112 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Grid: UIViewController {
+    
+    var rows: Int! = 50
+    var cols: Int! = 50
+    var cells: [[Cell]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.buildGridView()
+        
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self,
+                                                  selector: #selector(tick),
+                                                  userInfo: nil,
+                                                   repeats: true)
+    }
+    
+    func buildGridView() {
+        let screenWidth = UIScreen.mainScreen().bounds.size.width
+        
+        // Cells are square, i.e. width = height
+        let cellWidth = screenWidth / CGFloat(cols)
+        let cellHeight = cellWidth
+        
+        for row in 0..<rows {
+            var rowArray: [Cell] = []
+            for col in 0..<cols {
+                
+                let xOrigin = ((screenWidth / CGFloat(cols)) * CGFloat(col))
+                let yOrigin = ((screenWidth / CGFloat(rows)) * CGFloat(row))
+                
+                let frame = CGRect(x: xOrigin, y: yOrigin, width: cellWidth, height: cellHeight)
+                
+                let cell = Cell.init(frame, row: row, col: col)
+                rowArray.append(cell)
+                
+                self.view.addSubview(cell)
+            }
+            cells.append(rowArray)
+        }
+        
+    }
+    
+    func tick() {
+        var fates = Array<dispatch_block_t>()
+        for row in cells {
+            for cell in row {
+                self.findNeighbors(cell)
+                let fate = cell.fate()
+                fates.append(fate)
+            }
+        }
+        
+        for fate in fates {
+            fate()
+        }
+    }
+    
+    func findNeighbors(cell: Cell) {
+        cell.neighbors = []
+        
+        // Find top neighbors
+        if (cell.row > 0) {
+            let topCell = cells[cell.row - 1][cell.col]
+            cell.neighbors.append(topCell)
+            
+            if (cell.col > 0) {
+                let topLeftCell = cells[cell.row - 1][cell.col - 1]
+                cell.neighbors.append(topLeftCell)
+            }
+            
+            if (cell.col < (self.cols - 1)) {
+                let topRightCell = cells[cell.row - 1][cell.col + 1]
+                cell.neighbors.append(topRightCell)
+            }
+        }
+        
+        // Find neighbors side by side
+        if (cell.col < (self.cols - 1)) {
+            let rightCell = cells[cell.row][cell.col + 1]
+            cell.neighbors.append(rightCell)
+        }
+        
+        if (cell.col > 0) {
+            let leftCell = cells[cell.row][cell.col - 1]
+            cell.neighbors.append(leftCell)
+        }
+        
+        // Find bottom neighbors
+        if (cell.row < (self.rows - 1)) {
+            let bottomCell = cells[cell.row + 1][cell.col]
+            cell.neighbors.append(bottomCell)
+            
+            if (cell.col > 0) {
+                let bottomLeftCell = cells[cell.row + 1][cell.col - 1]
+                cell.neighbors.append(bottomLeftCell)
+            }
+            
+            if (cell.col < (self.cols - 1)) {
+                let bottomRightCell = cells[cell.row + 1][cell.col + 1]
+                cell.neighbors.append(bottomRightCell)
+            }
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,29 +122,4 @@ class ViewController: UIViewController {
     }
 
 
-}
-
-struct Cell {
-    var x: Int
-    var y: Int
-}
-
-struct Grid {
-    var rows: Int
-    var cols: Int
-    
-    var cells: [Cell] {
-        get {
-            var cellArray: [Cell] = []
-            for row in 0..<rows {
-                for col in 0..<cols {
-                    cellArray.append(Cell.init(x: row, y: col))
-                }
-            }
-            return cellArray
-        }
-    }
-    
-    
-    
 }
